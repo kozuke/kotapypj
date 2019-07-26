416fe297ce81cc3fa3c75b24a085c2339fea448f
kota ishizuka <kota.ishizuka1210@gmail.com>
2019-07-26 21:47:56 +0900

    ”We could not log you in”が出た時の対応

--- src/kozuke/pairs/automate-pairs.py
+++ src/kozuke/pairs/automate-pairs.py
@@ -19,6 +19,7 @@ _FACE_BOOK_LOGIN_BUTTON = '#registerBtn1'
 _FIRST_ACCESS_NUM = 1
 _MAX_ACCESS_NUM_BASE = int(environ['PAIRS_MAX_ACCESS_NUM'])
 _ADD_ACCESS_NUM = int(environ['PAIRS_ADD_ACCESS_NUM'])
+_PROFILE_PATH = r'/Users/kota-ishizuka/Library/Application Support/Google/Chrome'
 fail_count = 0
 
 # logger
@@ -30,7 +31,13 @@ def main():
     current_access_count = 0
 
     max_access_num = random.randint(_MAX_ACCESS_NUM_BASE, _MAX_ACCESS_NUM_BASE + _ADD_ACCESS_NUM)
-    driver = webdriver.Chrome()
+    options = webdriver.ChromeOptions()
+    # options.add_argument("user-data-dir=" + _PROFILE_PATH) # これでsession保てるはずだができない。
+
+    # このprofileを読み込ませることで「facebook.comが許可を求めています。」のアラートがなくなる。
+    prefs = {"profile.default_content_setting_values.notifications": 2}
+    options.add_experimental_option("prefs", prefs)
+    driver = webdriver.Chrome(chrome_options=options)
     driver.get(_PAIRS_LOGIN_URL)
     time.sleep(5)
     authenticate_facebook(driver)
@@ -64,14 +71,27 @@ def authenticate_facebook(driver):
     :param driver: chrome driver
     """
     driver.find_element_by_css_selector(_FACE_BOOK_LOGIN_BUTTON).click()
+
     time.sleep(5)
     handle_array = driver.window_handles
     driver.switch_to.window(handle_array[1])
     driver.find_element_by_css_selector('#email').send_keys(environ['FACEBOOK_EMAIL'])
     driver.find_element_by_css_selector('#pass').send_keys(environ['FACEBOOK_PASS'])
-    driver.find_element_by_css_selector('#u_0_0').click()
-    time.sleep(5)
-    driver.switch_to.window(handle_array[0])
+    try:
+        driver.find_element_by_css_selector('#u_0_0').click()
+        time.sleep(5)
+        driver.switch_to.window(handle_array[0])
+    except:
+        # "We could not log you in"が出た時の逃げ対応
+        time.sleep(3)
+        driver.find_element_by_css_selector('#u_0_2').click()
+        time.sleep(5)
+        driver.close()
+        time.sleep(3)
+        driver.switch_to.window(handle_array[0])
+        time.sleep(5)
+        driver.find_element_by_css_selector(_FACE_BOOK_LOGIN_BUTTON).click()
+        time.sleep(5)
 
 
 def get_random_list(driver):
@@ -100,6 +120,7 @@ def get_random_list(driver):
             logger.error('css NG')
             return exit(1)
         fail_count += 1
+        time.sleep(3)
         return get_random_list(driver)
 
 
