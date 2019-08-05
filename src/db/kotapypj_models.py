4d5e50e0d131bb6b2b0bb280bec748401652cca9
kota ishizuka <kota.ishizuka1210@gmail.com>
2019-08-05 23:49:17 +0900

    remove comment block

--- src/db/kotapypj_models.py
+++ src/db/kotapypj_models.py
@@ -9,3 +9,11 @@ class AccessLog(Base):
     id = Column('id', Integer, primary_key=True)
     access_time = Column('access_time', DATETIME, nullable=False, server_default=text('NOW()'))
     access_count = Column('access_count', Integer, nullable=False, server_default='0')
+
+
+# class AccessLogDetails(Base):
+#     __tablename__ = 'access_log_details'
+#     id = Column('id', Integer, primary_key=True)
+#     access_log_id = Column('id', Integer)
+#     access_time = Column('access_time', DATETIME, nullable=False, server_default=text('NOW()'))
+#     access_count = Column('access_count', Integer, nullable=False, server_default='0')
